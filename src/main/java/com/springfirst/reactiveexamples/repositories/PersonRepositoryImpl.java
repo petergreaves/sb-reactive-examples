71e393fae17f3e39bd618b4d5297b6b322cbdcc8
peter greaves <peter.greaves64@gmail.com>
2021-09-09 18:45:29 +0100

    StepVerifier in tests

--- src/main/java/com/springfirst/reactiveexamples/repositories/PersonRepositoryImpl.java
+++ src/main/java/com/springfirst/reactiveexamples/repositories/PersonRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.springfirst.reactiveexamples.repositories;
 
 import com.springfirst.reactiveexamples.domain.Person;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 public class PersonRepositoryImpl implements PersonRepository {
 
 
@@ -12,13 +14,24 @@ public class PersonRepositoryImpl implements PersonRepository {
     Person peter = Person.builder().id(3).firstName("Peter").lastName("Prune").build();
     Person sam = Person.builder().id(4).firstName("Sam").lastName("Antha").build();
 
+
     @Override
     public Mono<Person> getById(Integer id) {
-        return Mono.just(mike);
+
+        return findAll()
+                .filter(p -> p.getId() == id)
+                .next();
+
+
+//                .doOnError(throwable -> {
+//                    log.error("No such person with requested ID " + id + ", returning am empty Person");
+//                })
+//                .onErrorReturn(Person.builder().build());
+
     }
 
     @Override
     public Flux<Person> findAll() {
-        return Flux.just(mike,peter,sam,anne);
+        return Flux.just(mike, anne, peter, sam);
     }
 }
--- src/test/java/com/springfirst/reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/com/springfirst/reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -3,12 +3,15 @@ package com.springfirst.reactiveexamples.repositories;
 import com.springfirst.reactiveexamples.domain.Person;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.NoSuchElementException;
 
 class PersonRepositoryImplTest {
 
@@ -17,12 +20,11 @@ class PersonRepositoryImplTest {
 
     @BeforeEach
     void setUp() {
-
         personRepository = new PersonRepositoryImpl();
     }
 
     @Test
-    public void getByIdBlock() throws Exception {
+    public void getByIdBlock() {
 
         Mono<Person> personMono = personRepository.getById(1);
 
@@ -31,27 +33,24 @@ class PersonRepositoryImplTest {
     }
 
     @Test
-    public void getByIdSubscribe() throws Exception {
+    public void getByIdSubscribe() {
 
         Mono<Person> personMono = personRepository.getById(1);
-        personMono.subscribe(p -> {
-            System.out.println(p);
-        });
+
+        personMono.subscribe(System.out::println);
     }
 
     @Test
-    public void getByIdSubscribeWithMapTransform() throws Exception {
+    public void getByIdSubscribeWithMapTransform() {
 
         Mono<Person> personMono = personRepository.getById(1);
         personMono.map(person -> {
             return person.getFirstName().toUpperCase(Locale.ROOT);
-        }).subscribe(f -> {
-            System.out.println(f);
-        });
+        }).subscribe(System.out::println);
     }
 
     @Test
-    public void fluxFindFirstBlock() throws Exception {
+    public void fluxFindFirstBlock() {
 
         Flux<Person> personFlux = personRepository.findAll();
 
@@ -61,14 +60,11 @@ class PersonRepositoryImplTest {
     }
 
     @Test
-    public void fluxFindFirstSubscribe() throws Exception {
+    public void fluxFindFirstSubscribe() {
 
         Flux<Person> personFlux = personRepository.findAll();
 
-        personFlux.subscribe(p -> {
-
-            System.out.println(p);
-        });
+        personFlux.subscribe(System.out::println);
     }
 
     @Test
@@ -78,50 +74,92 @@ class PersonRepositoryImplTest {
 
         personFlux.map(p -> {
             return p.getFirstName().toUpperCase(Locale.ROOT);
-        }).subscribe(fn -> {
-            System.out.println(fn);
-        });
+        }).subscribe(System.out::println);
     }
 
     @Test
-    public void fluxToListMono() throws Exception {
+    public void fluxToListMono() {
 
         Mono<List<Person>> personListMono = personRepository.findAll().collectList();
 
-        personListMono.subscribe(list -> {
-            list.forEach(p -> {
+        StepVerifier.create(personListMono).expectNextCount(1).verifyComplete();
 
-                System.out.println(p);
-            });
+        personListMono.subscribe(list -> {
+            list.forEach(System.out::println);
 
         });
     }
 
     @Test
-    public void findByFluxFilter_success() throws Exception {
+    public void findByFluxFilter_success() {
 
         Flux<Person> personFlux = personRepository.findAll();
 
+        StepVerifier.create(personFlux).expectNextCount(4).verifyComplete();
+
         final Integer id = 2;
-        Mono<Person> personMono = personFlux.filter(p -> p.getId() == id).next(); //next, get next
-        personMono.subscribe(p -> {
-            System.out.println(p);
-        });
+        Mono<Person> personMono = personFlux.filter(p -> p.getId() == id).next(); //next, get next, fails silently
+
+        StepVerifier.create(personMono).expectNextCount(1).verifyComplete();
+        personMono.subscribe(System.out::println);
     }
 
     @Test
-    public void findByFluxFilterWithException_fail() throws Exception {
+    public void findByFluxFilterWithException_fail() {
 
         Flux<Person> personFlux = personRepository.findAll();
 
         final Integer id = 5;
-        Mono<Person> personMono = personFlux.filter(p -> p.getId() == id).single(); //expect and emit one, and only one
+        Mono<Person> personMono = personFlux.filter(p -> p.getId() == id).single(); //expect and emit one, and only one.
+        //throws a java.util.NoSuchElementException if filter returns not exactly 1
+
+        StepVerifier.create(personMono).expectError(NoSuchElementException.class);
 
         personMono.doOnError(throwable -> {
-            System.out.println("Boom!");
+            System.out.println("Boom! with a " + throwable.getClass());
         })
-                .onErrorReturn(Person.builder().id(id).build()).subscribe(p -> {
-            System.out.println(p);
+                .onErrorReturn(Person.builder().id(id).build()).subscribe(System.out::println);
+    }
+
+    @Test
+    @DisplayName("Flux returns right number")
+    public void findAllReturnsFour() {
+
+        final int expected = 4;
+        Flux<Person> allPeople = personRepository.findAll();
+        StepVerifier.create(allPeople).expectNextCount(expected).verifyComplete();
+    }
+
+
+    @Test
+    public void findByIDWhereIDExists() {
+        final Integer id = 3;
+
+        Mono<Person> person = personRepository.getById(id);
+
+        Person peter = Person.builder().id(3).firstName("Peter").lastName("Prune").build();
+
+        StepVerifier.create(person).expectNext(peter).verifyComplete();
+        person.subscribe(p -> {
+            Assertions.assertSame("Peter", p.getFirstName());
+        });
+
+    }
+
+    @Test
+    @DisplayName("Search by Id with no match returns an empty Person")
+    public void findByIDWhereIDDoesNotExist() {
+        final Integer id = 6;
+
+        Mono<Person> person = personRepository.getById(id);
+
+        StepVerifier.create(person).expectNextCount(0).verifyComplete();
+        // or StepVerifier.create(person).verifyComplete();
+        person.subscribe(p -> {
+            Assertions.assertAll(
+                    () -> Assertions.assertNull(p.getId()),
+                    () -> Assertions.assertNull(p.getFirstName()),
+                    () -> Assertions.assertNull(p.getLastName()));
         });
     }
 }
